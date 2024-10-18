d128e84271c310914adcc47bd59fbf6d5718cc18
VuThiNgocMai <vuthingocmai2018@gmail.com>
2024-10-18 07:44:00 +0700

    Create_Project

---
+++ VuThiNgocMai430/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VuThiNgocMai430.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
